5d8808633b64e366672e0e8e45213f2e72bef92b
BuildTools <unconfigured@null.spigotmc.org>
2023-09-13 19:00:38 -0300

    css-html-noscripts

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+// seleção de elementos
+const generatePasswordBtn = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// funções
+
+// eventos
+
+generatePasswordBtn.addEventListener("click", () => {
+    console.log("teste");
+});
